c14a997b6db15dfe57283b63554185c9a897f9c4
agubelu <agustinbd3@gmail.com>
2022-12-10 14:16:32 +0000

    📺 Day 10 📺
    Refactor, remove some code

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,28 +1,20 @@
 use crate::{Solution, SolutionPair};
-use Instruction::*;
 use itertools::Itertools;
 use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-enum Instruction {
-    Noop,
-    Addx(i32)
-}
-
 pub fn solve() -> SolutionPair {
-    let instrs = read_to_string("input/day10.txt").unwrap()
-        .lines()
-        .map(Instruction::from_line)
-        .collect_vec();
+    let input = read_to_string("input/day10.txt").unwrap();
 
     let mut reg = 1;
     let mut cycle = 1;
     let mut sol1 = 0;
     let mut screen = [[' '; 40]; 6];
 
-    for instr in &instrs {
-        for _ in 0..instr.cycles() {
+    for line in input.lines() {
+        let spl: Vec<&str> = line.split(' ').collect();
+        for _ in 0..spl.len() { // conveniently, the number of cycles is the same as the instruction size
             // Update signal strength for part 1
             if (cycle - 20) % 40 == 0 {
                 sol1 += cycle * reg;
@@ -40,8 +32,8 @@ pub fn solve() -> SolutionPair {
             cycle += 1;
         }
 
-        if let Addx(n) = instr {
-            reg += n;
+        if spl.len() == 2 {
+            reg += spl[1].parse::<i32>().unwrap();
         }
     }
 
@@ -49,22 +41,4 @@ pub fn solve() -> SolutionPair {
     let sol2 = format!("\n{}", display);
 
     (Solution::from(sol1), Solution::from(sol2))
-}
-
-impl Instruction {
-    pub fn from_line(line: &str) -> Self {
-        let spl = line.split(' ').collect_vec();
-        match spl[0] {
-            "noop" => Noop,
-            "addx" => Addx(spl[1].parse().unwrap()),
-            _ => unreachable!()
-        }
-    }
-
-    pub fn cycles(&self) -> u32 {
-        match self {
-            Noop => 1,
-            Addx(_) => 2,
-        }
-    }
 }
