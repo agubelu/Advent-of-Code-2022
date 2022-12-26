456bf1e98ea43b27a602fe0d3ed6651d8d6d9c53
agubelu <agustinbd3@gmail.com>
2022-12-26 16:13:56 +0100

    🍪 Day 01 🍪
    Use the platform-specific double newline separator for compatibility with Windows

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,17 +1,18 @@
 use crate::{Solution, SolutionPair};
+use crate::etc::utils::DOUBLE_NEWLINE;
 use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
     let mut calories: Vec<u32> = read_to_string("input/day01.txt").unwrap()
-        .split("\n\n")
+        .split(DOUBLE_NEWLINE)
         .map(|elf| elf.lines().map(|x| x.parse::<u32>().unwrap()).sum())
         .collect();
 
-    calories.sort_unstable_by(|a, b| b.cmp(a));
+    calories.sort_by(|a, b| b.cmp(a));
     let sol1 = calories[0];
-    let sol2 = calories[0..3].iter().sum();
+    let sol2: u32 = calories[0..3].iter().sum();
 
-    (Solution::U32(sol1), Solution::U32(sol2))
+    (Solution::from(sol1), Solution::from(sol2))
 }
