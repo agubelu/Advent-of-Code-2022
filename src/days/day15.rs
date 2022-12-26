38c0fa4f5d4f11e4c424b67743ddd78d8de37d41
agubelu <agustinbd3@gmail.com>
2022-12-26 16:04:44 +0100

    ♦ Day 15 ♦
    Big refactor, runtime down from 11.5s to ~55ms

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,17 +1,13 @@
-// TO-DO: i'm catching up after being away and this code is
-// a bit of a mess as i was going through trial and error,
-// a nice refactor will happen soon
+use std::fs::read_to_string;
+
+use rayon::prelude::*;
+use scanf::sscanf;
 
 use crate::{Solution, SolutionPair};
 use crate::etc::coords::Coords;
-use rustc_hash::FxHashMap;
-use itertools::Itertools;
-use scanf::sscanf;
-use std::cmp::Ordering;
-use std::fs::read_to_string;
 use BoundType::*;
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 type Pos = Coords<i64>;
 const COUNT_ROW: i64 = 2_000_000;
@@ -19,92 +15,98 @@ const PART2_BOUND: i64 = 4_000_000;
 
 struct SensorInfo {
     position: Pos,
-    nearest_beacon: Pos,
+    range: i64,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct RangeBound {
     pos: i64,
     kind: BoundType,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum BoundType { Start, End }
 
 pub fn solve() -> SolutionPair {
-    let data = read_to_string("input/day15.txt").unwrap()
+    let sensors: Vec<SensorInfo> = read_to_string("input/day15.txt").unwrap()
         .lines()
         .map(load_line_info)
-        .collect_vec();
+        .collect();
 
-    // Store all positions where we are sure that no beacon exists, that is,
-    // those that are at the same distance or closer to the sensor than the
-    // closest beacon
-    let mut no_beacons = FxHashMap::default();
-    data.iter().for_each(|x| update_no_beacons(x, &mut no_beacons));
+    // Part 1: count the spaces in the provided row
+    let sol1 = count_spaces_row(&sensors, COUNT_ROW, None).0;
 
-    let sol1 = count_range_sizes(no_beacons.get_mut(&COUNT_ROW).unwrap()).0;
-    let beacon_pos = (0..=PART2_BOUND).map(|y| (y, count_range_sizes(no_beacons.get_mut(&y).unwrap()).1))
-        .find(|(_, opt)| opt.is_some())
-        .unwrap();
-
-    let sol2 = beacon_pos.1.unwrap() * 4000000 + beacon_pos.0;
+    // Part 2: find out which row contains a single gap
+    let sol2 = (0..=PART2_BOUND).into_par_iter()
+        .find_map_any(|y| {
+            let found = count_spaces_row(&sensors, y, Some((0, PART2_BOUND))).1;
+            found.map(|x| x * 4_000_000 + y)
+        }).unwrap();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
 
-fn count_range_sizes(ranges: &mut[RangeBound]) -> (usize, Option<i64>) {
+// Fins out how many guaranteed sensor-free spaces there are in a row,
+// optionally bounded to a range of x-coordinates, and also returns the
+// only space where a sensor could be for part 2
+fn count_spaces_row(sensors: &[SensorInfo], row: i64, bounds: Option<(i64, i64)>) -> (i64, Option<i64>) {
+    let mut ranges = vec![];
+
+    for &SensorInfo { position, range } in sensors {
+        let y_diff = (position.y - row).abs();
+
+        // If this sensor is out of range for the provided row, skip it
+        if y_diff > range {
+            continue;
+        }
+
+        let x_margin = range - y_diff;
+        let start = position.x - x_margin;
+        let end = position.x + x_margin;
+
+        // Only add this range if it's within bounds, if they are provided
+        if let Some((min, max)) = bounds {
+            if end < min || start > max {
+                continue;
+            }
+        }
+        
+        ranges.push(RangeBound { pos: position.x - x_margin, kind: Start });
+        ranges.push(RangeBound { pos: position.x + x_margin, kind: End });
+    }
+
+    count_range_sizes(&mut ranges)
+}
+
+// Algorithm that counts the number of distinct elements in overlapping number
+// ranges in O(n), from https://stackoverflow.com/questions/20553345/length-of-union-of-ranges
+// Slightly modified to find length 1 gaps for part 2
+fn count_range_sizes(ranges: &mut[RangeBound]) -> (i64, Option<i64>) {
     ranges.sort();
 
-    let mut start = ranges[0].pos;
-    let mut end = i64::MAX;
-    let mut counter = 0;
-    let mut res = 0;
+    let (mut start, mut end) = (ranges[0].pos, i64::MAX);
+    let (mut counter, mut res) = (0, 0);
     let mut part2 = None;
 
     for &RangeBound { pos, kind } in ranges.iter() {
-        if kind == Start {
-            if counter == 0 {
-                start = pos;
-                if pos > 0 && pos <= PART2_BOUND && start == end + 2 {
-                    part2 = Some(pos - 1);
-                }
+        if counter == 0 {
+            start = pos;
+            if pos > 0 && pos <= PART2_BOUND && start == end + 2 {
+                part2 = Some(pos - 1);
             }
-            counter += 1;
-        } else {
-            counter -= 1;
-            if counter == 0 {
-                end = pos;
-                res += end - start;
-            }
+        }
+
+        counter += if kind == Start { 1 } else { -1 };
+
+        if counter == 0 {
+            end = pos;
+            res += end - start;
         }
     }
 
-    (res as usize, part2)
+    (res, part2)
 }
 
-fn update_no_beacons(data: &SensorInfo, no_beacons: &mut FxHashMap<i64, Vec<RangeBound>>) {
-    let origin = data.position;
-    let dist = (origin.x - data.nearest_beacon.x).abs() + (origin.y - data.nearest_beacon.y).abs();
-
-    for y_diff in 0..=dist {
-        let x_diff = dist - y_diff;
-
-        for sign in [1, -1] {
-            let y_pos = origin.y + y_diff * sign;
-            
-            if !(0..=PART2_BOUND).contains(&y_pos) {
-                continue;
-            }
-            let x_range_start = RangeBound{pos: origin.x - x_diff, kind: Start};
-            let x_range_end = RangeBound{pos: origin.x + x_diff, kind: End};
-
-            let entry = no_beacons.entry(y_pos).or_insert(vec![]);
-            entry.push(x_range_start);
-            entry.push(x_range_end);
-        }
-    }
-}
 
 fn load_line_info(line: &str) -> SensorInfo {
     let (mut x_sensor, mut y_sensor) = (0, 0);
@@ -116,21 +118,6 @@ fn load_line_info(line: &str) -> SensorInfo {
     ).unwrap();
 
     let position = Pos::new(x_sensor, y_sensor);
-    let nearest_beacon = Pos::new(x_beacon, y_beacon);
-    SensorInfo { position, nearest_beacon }
-}
-
-impl Ord for RangeBound {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.pos.cmp(&other.pos) {
-            Ordering::Equal => self.kind.cmp(&other.kind),
-            x => x,
-        }
-    }
-}
-
-impl PartialOrd for RangeBound {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+    let range = (x_beacon - x_sensor).abs() + (y_beacon - y_sensor).abs();
+    SensorInfo { position, range }
 }
