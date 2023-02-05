e3aa4f04ae4ef7dc5ee496c80bb99691d18b4a4a
agubelu <agustinbd3@gmail.com>
2023-02-06 00:31:26 +0100

    ⭐ Day 25 ⭐
    yippee

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -1,13 +1,45 @@
-#![allow(unused)]
 use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+const SNAFU_CHARS: [char; 5] = ['0', '1', '2', '=', '-'];
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+pub fn solve() -> SolutionPair {
+    let input = read_to_string("input/day25.txt").unwrap();
+    let code = input.lines().map(snafu2dec).sum();
+
+    let sol1 = dec2snafu(code);
+    let sol2 = "🎄❄️ Merry Christmas! ❄️🎄";
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn dec2snafu(mut number: i64) -> String {
+    let mut res = String::new();
+
+    while number > 0 {
+        let rem = number % 5;
+        res = format!("{}{}", SNAFU_CHARS[rem as usize], res);
+        if rem >= 3 {
+            number += 5;
+        }
+        number /= 5;
+    }
+
+    res
+}
+
+fn snafu2dec(number: &str) -> i64 {
+    number.chars().rev().enumerate()
+          .map(|(i, ch)| 5_i64.pow(i as u32) * snafu_digit(ch))
+          .sum()
+}
+
+fn snafu_digit(ch: char) -> i64 {
+    match ch {
+        '=' => -2,
+        '-' => -1,
+         x  => x.to_digit(10).unwrap() as i64,
+    }
 }
